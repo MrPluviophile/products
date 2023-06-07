5098a9910ebbb6f844a960365b8902377fb550ee
MrPluviophile <sisnmi91@gmail.com>
2023-06-07 22:54:37 +0800

    add read file

--- products.py
+++ products.py
@@ -1,4 +1,13 @@
 products = []
+with open('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if 'Product,Price' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name, price])
+print(products)
+
+# Ask user to input
 while True:
 	name = input('Please input product name: ')
 	if name == 'q':
@@ -8,10 +17,12 @@ while True:
 	products.append([name, price])
 print(products)
 
+# print all the buying record
 for p in products:
 	print(p[0], 's price is', p[1])
 
-with open('product.csv', 'w', encoding='utf-8') as f:
+# write into file
+with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write('Product,Price\n')
 	for p in products:
 		f.write(p[0] + ',' + str(p[1]) + '\n')
